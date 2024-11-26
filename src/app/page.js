5ddd4f6a98bff5edc9c738c3d5da8314163472a7
lelezinhe2606 <leticia.ferreira@estudante.ifro.edu.br>
2024-11-26 15:40:59 -0400

    up

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import Image  from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1>Olivia Rodrigo1</h1>
+      <h1>Olivia Rodrigo</h1>
       <Image className={styles.img} src='/images/oliviashow.jpg' 
       alt="Imagem do Música" width={300} height={450}/>    
  
